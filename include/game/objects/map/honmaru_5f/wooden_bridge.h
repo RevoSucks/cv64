8b24e218a23ba1f738c89047d186547eefa33d5b
moisesPC <moisesapc64@gmail.com>
2025-08-18 11:43:21 +0100

    Prepare headers

---
+++ include/game/objects/map/honmaru_5f/wooden_bridge.h
@@ -0,0 +1,26 @@
+#ifndef HONMARU_5F_WOODEN_BRIDGE_H
+#define HONMARU_5F_WOODEN_BRIDGE_H
+
+#include "actor.h"
+
+// ID 0x01D9
+typedef struct WoodenBridge {
+    ObjectHeader header;
+    u8 field_0x20[4];
+    Model* model;
+    u8 field_0x28[64];
+    MapActorModel* map_actor_model;
+    u8 field_0x6C[4];
+    ActorConfig* settings;
+} WoodenBridge;
+
+void WoodenBridge_Entrypoint(WoodenBridge* self);
+void WoodenBridge_Init(WoodenBridge* self);
+void WoodenBridge_Calc(WoodenBridge* self);
+
+typedef void (*WoodenBridgeFunc)(WoodenBridge*);
+
+extern const u32 HONMARU_5F_WOODEN_BRIDGE_REINHARDT_DL;
+extern const u32 HONMARU_5F_WOODEN_BRIDGE_CARRIE_DL;
+
+#endif
