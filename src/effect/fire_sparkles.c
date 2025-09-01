ee05443bcd13291e1a60eb53ee9b5897e28ea49c
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2025-08-31 22:39:22 -0400

    fix(effect): use `BITS` macros and explicit casts

--- src/effect/fire_sparkles.c
+++ src/effect/fire_sparkles.c
@@ -32,46 +32,52 @@ void FireSparkles_Init(FireSparkles* self) {
     EffectAnimationInfo* anim_info;
     Model* model;
 
-    if (effect_init(self)) {
-        (*effect_initParams)(self, EFFECT_ID_FIRE_SPARKLES, ptr_effectMgr->display_camera, 0);
+    if (effect_init((Effect*) self)) {
+        (*effect_initParams)(
+            (Effect*) self, EFFECT_ID_FIRE_SPARKLES, ptr_effectMgr->display_camera, 0
+        );
     }
 
     visual_info        = self->visual_info;
-    visual_info->model = self->model = model = effect_getModel(self);
+    visual_info->model = self->model = model = effect_getModel((Effect*) self);
 
-    if (1) {
-        if (model == NULL) {
-            GO_TO_FUNC_NOW(self, FireSparkles_functions, FIRE_SPARKLES_DESTROY);
-            return;
-        }
-
-        model->material_dlist = (*osVirtualToPhysical)(FireSparkles_material_dlist);
-        model->dlist          = FIG_APPLY_VARIABLE_TEXTURE_AND_PALETTE((u32) &FIRE_SPARKLES_DLIST);
-        model->assets_file    = NI_COMMON_GAMEPLAY_EFFECTS;
-        effect_updateGraphicParams(self);
-
-        anim_info = &visual_info->anim_info;
-        effectAnimationInfo_create(anim_info, 0);
-        effectAnimationInfo_init(anim_info, model, visual_info->max_frame_speed, 1.0f);
-        if (visual_info->flags & EFF_VISUAL_FLAG_NO_VARIABLE_TRANSPARENCY) {
-            visual_info->anim_info.visual_flags &= ~EFF_ANIM_VISUAL_FLAG_VARIABLE_TRANSPARENCY;
-        }
-
-        effect_createPointLight(self);
-        (*object_curLevel_goToNextFuncAndClearTimer)(
-            self->header.current_function, &self->header.function_info_ID
-        );
+    if (!TRUE) {
+        // Trivial condition needed for matching
+        return;
     }
+
+    if (model == NULL) {
+        GO_TO_FUNC_NOW(self, FireSparkles_functions, FIRE_SPARKLES_DESTROY);
+        return;
+    }
+
+    model->material_dlist = (*osVirtualToPhysical)(FireSparkles_material_dlist);
+    model->dlist          = FIG_APPLY_VARIABLE_TEXTURE_AND_PALETTE((u32) &FIRE_SPARKLES_DLIST);
+    model->assets_file    = NI_COMMON_GAMEPLAY_EFFECTS;
+    effect_updateGraphicParams((Effect*) self);
+
+    anim_info = &visual_info->anim_info;
+    effectAnimationInfo_create(anim_info, 0);
+    effectAnimationInfo_init(anim_info, model, visual_info->max_frame_speed, 1.0f);
+    if (BITS_HAS(visual_info->flags, EFF_VISUAL_FLAG_NO_VARIABLE_TRANSPARENCY)) {
+        BITS_UNSET(visual_info->anim_info.visual_flags, EFF_ANIM_VISUAL_FLAG_VARIABLE_TRANSPARENCY);
+    }
+
+    effect_createPointLight((Effect*) self);
+    (*object_curLevel_goToNextFuncAndClearTimer)(
+        self->header.current_function, &self->header.function_info_ID
+    );
 }
 
 void FireSparkles_Loop(FireSparkles* self) {
-    s32 should_despawn = effect_loop(self);
+    s32 should_despawn = effect_loop((Effect*) self);
 
-    if (should_despawn) {
-        (*object_curLevel_goToNextFuncAndClearTimer)(
-            self->header.current_function, &self->header.function_info_ID
-        );
-    }
+    if (!should_despawn)
+        return;
+
+    (*object_curLevel_goToNextFuncAndClearTimer)(
+        self->header.current_function, &self->header.function_info_ID
+    );
 }
 
 void FireSparkles_Destroy(FireSparkles* self) {
